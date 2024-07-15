285798a31bc8111e79d9cdc2b1a6b7fcbea1bebb
BenchPress200 <jms0538@naver.com>
2024-07-15 11:12:32 +0900

    init

---
+++ src/main/java/com/oss/beellage/BeellageApplication.java
@@ -0,0 +1,13 @@
+package com.oss.beellage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeellageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeellageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/oss/beellage/BeellageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oss.beellage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeellageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
