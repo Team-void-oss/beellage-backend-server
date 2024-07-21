0a99a423d769784b7c1616e0b9a0500e50f4b619
Haisley Jang <98401161+Ssun2zang@users.noreply.github.com>
2024-07-21 20:40:16 +0900

    delete: team, user entity

--- src/main/java/com/oss/beellage/team_haisley/repository/TeamRepository.java
+++
@@ -1,8 +0,0 @@
-package com.oss.beellage.team_haisley.repository;
-
-import com.oss.beellage.team_haisley.Team;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TeamRepository extends JpaRepository<Team, Long> {
-}
-
--- src/main/java/com/oss/beellage/user_haisley/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.oss.beellage.user_haisley.repository;
-
-import com.oss.beellage.user_haisley.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
